38685061ab5a8e90f06307c694e0ac5badb6158f
leal95 <gabrielleal51@gmail.com>
2020-04-19 20:27:59 -0300

    Criando Login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,10 @@
 const express = require ('express');
 const UsuariosController = require ('./controllers/UsuariosController');
+const SessionController = require ('./controllers/SessionController');
 const routes = express.Router();
 
-routes.get('/usuarios', UsuariosController.index);
-
-routes.post('/usuarios', UsuariosController.create);
+routes.get('/usuarios', UsuariosController.index); //listagem
+routes.post('/usuarios', UsuariosController.create); //cadastro
+routes.post('/session', SessionController.create); //login
 
 module.exports = routes;
