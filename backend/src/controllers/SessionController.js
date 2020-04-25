f4b05f5638f90c09374295d093dd58d6482d96df
leal95 <gabrielleal51@gmail.com>
2020-04-24 23:34:22 -0300

    arrumando retorno do login

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -4,15 +4,17 @@ module.exports = {
     async create (request, response) {
       const {id} = request.body;
 
+      try{
       const usuario = await connection('usuarios')
       .where('id', id)
       .select('email')
       .first()
 
-      if (!usuario){
-        return response.status(400).json({ error: 'Usuário não encontrado' });
-    }
+      return response.json(usuario);
+      }
 
-    return response.json(usuario);
+      catch(err){
+        response.json({message:err});
+}
 }
 }
