58ff5bd11f87220381cd1eacaae6de229b2e74d7
leal95 <gabrielleal51@gmail.com>
2020-05-05 23:28:34 -0300

    Começando Update

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,12 @@
 const express = require ('express');
 const UsuariosController = require ('./controllers/UsuariosController');
+const ProfileController = require ('./controllers/ProfileController');
 const SessionController = require ('./controllers/SessionController'); //importando express e controllers
 
 const routes = express.Router(); //desacoplando Router do express e alocando na nova variável
 
-routes.get('/sessions', SessionController.login); //login (criando uma sessão)
+routes.put('/profile', ProfileController.edit);
+routes.post('/sessions', SessionController.login); //login (criando uma sessão)
 routes.get('/usuarios', UsuariosController.index); //listagem
 routes.post('/usuarios', UsuariosController.create); //cadastro
 
