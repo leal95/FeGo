4f2c2371966cbf5f846311a5f8e8ae9c12c123e0
Fernando Penha <FZPenha@gmail.com>
2020-05-07 17:03:32 -0300

    removendotelainicial

--- frontend/src/pages/TelaInicial/index.js
+++ frontend/src/pages/TelaInicial/index.js
@@ -16,45 +16,7 @@ export default function Cadastro() {
     };
 
     return(
-        <View style={styles.container}>
-            <Feather name="arrow-left" size={24} color="#999" onPress={navigateToLogin} />
-            <View style={styles.header}>
-                <Text style={styles.headerText}>Olá, {dados.nome}</Text>
-            </View>
-
-            <View style={styles.user}>
-                <View style={styles.userFoto}></View>
-                <Text style={styles.userName}> {dados.nome} {dados.sobrenome}</Text>
-            </View>
-
-            <View style={styles.botoesIniciais}>
-                <TouchableOpacity style={styles.botaoInicial}>
-
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.botaoInicial}>
-                    
-                </TouchableOpacity>
-            </View>
-
-            <View style={styles.botoesIniciais}>
-                <TouchableOpacity style={styles.botaoInicial}>
-
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.botaoInicial}>
-                    
-                </TouchableOpacity>
-            </View>
-
-            <View style={styles.botoesCaronas}>
-                <TouchableOpacity style={styles.botaoCarona}>
-                    <Text style={styles.textCaronas}>Procurar</Text>
-                    <Text style={styles.textCaronas}>Carona</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.botaoCarona}>
-                    <Text style={styles.textCaronas}>Oferecer</Text>
-                    <Text style={styles.textCaronas}>Carona</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
+        <>
+        </>
     )
 }
