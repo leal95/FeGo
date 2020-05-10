f994303be0160dd8469b87ed3b15cf58397bd293
Fernando Penha <FZPenha@gmail.com>
2020-05-10 15:15:26 -0300

    encurtamentos em profilecontroller

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -3,30 +3,21 @@ const connection = require ('../database/connection'); //importando conexão com
 module.exports={
     async edit (request, response){
        
-        const {email} = request.body
-        const {senha} = request.body;
-        const {ra} = request.body;
-        const {nome} = request.body;
-        const {sobrenome} = request.body;
-        const {numTelefone} = request.body;
-        const {apelido} = request.body;
-        const {fumante} = request.body;
-        const {curso} = request.body;
-        const {musica} = request.body; //requisitando dados de mudança
+        const {email, senha, ra, nome, sobrenome, numTelefone, apelido, fumante, curso, musica} = request.body
 
         try{
             await connection('usuarios')
             .where('email', {email})
             .update({
-                senha: {senha},
-                ra: {ra},
-                nome: {nome},
-                sobrenome: {sobrenome},
-                numTelefone: {numTelefone},
-                apelido: {apelido},
-                fumante: {fumante},
-                curso: {curso},
-                musica: {musica}
+                senha,
+                ra,
+                nome,
+                sobrenome,
+                numTelefone,
+                apelido,
+                fumante,
+                curso,
+                musica,
             })
 
             response.json({ email, senha, ra, nome, sobrenome, numTelefone, apelido, fumante, curso, musica });
