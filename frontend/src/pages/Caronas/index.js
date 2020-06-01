3ee57bbb3db37d62398caa0865e75ce4159a2cb4
Fernando Penha <FZPenha@gmail.com>
2020-06-01 19:43:10 -0300

    Funcionalidade e UXI: visu caronas

--- frontend/src/pages/Caronas/index.js
+++ frontend/src/pages/Caronas/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { View, Text, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
 import { Feather } from '@expo/vector-icons';
@@ -10,34 +10,59 @@ export default function Caronas() {
     const route = useRoute();
     const navigation = useNavigation();
 
+    const [caronas, setCaronas] = useState([]);
+
     const dados = route.params.dados;
 
     function navigateToTelaInicial() {
         navigation.navigate('TelaInicial', {dados});
     };
 
+    function detalheCarona () {
+        alert('Tela ainda não existe!');
+    };
+
+    async function loadCaronas() {
+        const response = await api.get('/caronas');
+
+        setCaronas = response.data;
+    }
+
+    useEffect(() => {
+        loadCaronas();
+    }, [])
+
+    //flatlist com o codigo certo de dados (nao temos, entao mantive como comentario)
+
+    /* <FlatList style={styles.CaronasList}
+            data = {caronas}
+            keyExtractor={carona => String(carona.id)}
+            showsVerticalScrollIndicator = {false}
+            renderItem = {({item: carona})=>(
+                <TouchableOpacity style={styles.Caronas}
+                onPress={() => detalheCarona()}>
+                <View style={styles.userFoto}></View>
+                <View style={styles.CaronasInfo}>
+                <Text style={styles.CaronasText}> De: {carona.origem} </Text>
+                <Text style={styles.CaronasText}> Para: {carona.destino}  </Text>
+                <Text style={styles.CaronasText}> Horário: {carona.hora}:{carona.minuto} </Text>
+                <Text style={styles.CaronasText}> Data: {carona.dia}/{carona.mes}/{carona.ano} </Text>
+                </View>
+                </TouchableOpacity>
+            )}
+            /> */ 
     
     return(
         <View style={styles.container}>
-                <TouchableOpacity style={styles.botaoVoltar}>
-                    <Feather name="arrow-left" size={24} color="#858585" onPress = {navigateToTelaInicial}/>
-                </TouchableOpacity>
+                <Feather name="arrow-left" size={24} color="#858585" onPress = {navigateToTelaInicial}/>
             <View style={styles.header}>
                 <Text style={styles.headerText}>Caronas</Text>
             </View>
-
-            <KeyboardAvoidingView behavior="padding" style={styles.Buscar}>
-            <TouchableOpacity><Feather name="search" size={40}/></TouchableOpacity>
-                <TextInput
-                    style={styles.inputText}
-                    placeholder="Buscar Carona"
-                    autoCorrect={false}
-                />
-            </KeyboardAvoidingView>
             
             <FlatList style={styles.CaronasList}
             data = {[1,2,3,4,5,6,7,8]}
-            keyExtractor={incident => String(incident)}
+            keyExtractor={carona => String(carona)}
+            showsVerticalScrollIndicator = {false}
             renderItem = {()=>(
                 <TouchableOpacity style={styles.Caronas}>
                 <View style={styles.userFoto}></View>
--- frontend/src/pages/Caronas/styles.js
+++ frontend/src/pages/Caronas/styles.js
@@ -2,56 +2,6 @@ import { StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 
 export default StyleSheet.create({
-    botaoCadastrar: {
-    },
-
-    textoCadastrese:{
-        color: '#c4c4c4',
-        fontSize: 14,
-        justifyContent: 'center',
-        alignItems: 'center',
-        
-    },
-    
-    
-    botaoCadastrarText: {
-        color: '#347EBF',
-        fontSize: 16,
-        textDecorationLine: "underline",
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-
-    botaoVoltar:{
-        position: 'absolute',
-        left:15, top:30,
-        },
-        
-    botaoLogin: {
-        backgroundColor: '#347EBF',
-        borderRadius: 50,
-        height: 50,
-        width: 250,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-
-    botaoLoginText: {
-        color: '#fff',
-        fontSize: 24,
-        letterSpacing: 3,
-    },
-
-    botoes: {
-        alignItems: 'center'
-    },
-
-    textosfinais:{
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center'
-    },
-
     container: {
         flex: 1,
         backgroundColor: 'white',
@@ -59,7 +9,8 @@ export default StyleSheet.create({
     },
 
     header: {
-        paddingLeft: 32,
+        alignItems: 'center',
+        marginBottom: 20,
     },
 
     headerText: {
@@ -68,24 +19,15 @@ export default StyleSheet.create({
         color: '#818181'
     },
 
-    inputs: {
-        alignItems: 'center',
-        height: 375,
-        justifyContent: 'center'
-    },
-
     Buscar:{
         alignSelf:"center",
         alignItems:"center",
         flexDirection:"row",
         marginTop:30
-
     },//Estilo do Buscar
 
     CaronasList:{
         alignSelf:"center",
-        marginTop:50,
-        marginBottom:50,
     },//Estilo da FlatList
 
     userFoto: {
@@ -97,14 +39,15 @@ export default StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#aaa',
-        marginTop: 30
+        marginTop: 30,
+        marginLeft: 10,
     }, //Estilos da foto de Perfil
 
 
     Caronas: {
         backgroundColor:"#347EBF",
-        width: 330,
-        height:155,
+        width: 300,
+        height:150,
         marginBottom:20,
         borderRadius:30,
         flexDirection:"row"
@@ -113,26 +56,12 @@ export default StyleSheet.create({
     CaronasText:{
         alignSelf:"flex-end",
         marginTop:20,
-        marginRight:20
+        marginRight:20,
+        fontWeight: 'bold',
+        color: '#ddd',
     },//Estilo dos Textos das Informações das caronas
     
     CaronasInfo:{
         marginLeft:40
     },//Posicionamento dos Textos
-    
-    
-    inputText: {
-        backgroundColor: '#fff',
-        borderColor: '#347EBF',
-        borderWidth: 2,
-        borderRadius: 10,  
-        height: 50,
-        width: 300,
-        padding: 10,
-    },
-
-    inputTextHeader: {
-        fontSize: 19,
-        color: '#A2A2A2',
-    }
 });
